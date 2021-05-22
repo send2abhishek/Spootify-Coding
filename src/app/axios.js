85d53f1edeb87cda57ae231e8df656ed268aa8d3
Kumar, Abhishek <abhishek.kumar3@tyson.com>
2021-05-22 21:57:35 +0530

    Fix Readme file

--- src/app/axios.js
+++ src/app/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // import configuration from "config";
-let access_token =
-  "BQBBhuvFxWuC5r-Gt1u04Mqz0gbSg2Z-YoR7r_Pdb2DTfUcPt5OHhcjJqbHNQucN853r6fuvIkmUQX6rpj8";
+let access_token = "insert access token here";
 let axiosObject;
 axios.defaults.headers.common["Authorization"] = `Bearer ${access_token}`;
 if (process.env.NODE_ENV === "development") {
